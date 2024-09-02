514415b59745f17595378af25c5d578b1bf88fc3
haiq0001 <haiq0001@stud.kea.dk>
2024-09-02 19:30:34 +0200

    Update DoWhileLoop.java

--- src/DoWhileLoop.java
+++ src/DoWhileLoop.java
@@ -35,10 +35,30 @@ public class DoWhileLoop {
 
         //Skriv din kode herunder: ----------------------------------------------------------------------------------------------
 
+     
+import java.util.Scanner;
 
+public class DoWhileLoop {
 
+    public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        boolean okay = false;
 
+        while (!okay) {
+            System.out.print("Indtast et heltal: ");
+            if (scanner.hasNextInt()) {
+                okay = true;
+            } else {
+                okay = false;
+                scanner.next();
+                System.out.println("Du har ikke indtastet et heltal.");
+            }
+        }
+                int number = scanner.nextInt();
+                System.out.println("Tak");
+            }
+        }
 
 
         //------------------------------------------------------------------------------------------------------------------------
